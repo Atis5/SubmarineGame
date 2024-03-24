37f923eba2e9d65ff0e633ef9afa540cff558c3a
Chris <fear123450pl@o2.pl>
2024-03-24 22:34:57 +0100

    Project added to the repository

---
+++ Submarine Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float PlayerSpeed;
+    public float GroundDrag;
+
+    [Header("Ground Check")]
+    public float PlayerHeight;
+    public LayerMask GroundObjects;
+    private bool Grounded;
+
+    public Transform Orientation;
+
+    private float HorizontalInput;
+    private float VerticalInput;
+
+    Vector3 MoveDirection;
+
+    Rigidbody RigidBody;
+
+    private void Start()
+    {
+        RigidBody = GetComponent<Rigidbody>();
+        RigidBody.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        // Check if Player is on the ground
+        Grounded = Physics.Raycast(transform.position, Vector3.down, PlayerHeight * 0.5f + 0.2f, GroundObjects);
+
+        MyInput();
+        SpeedControl();
+
+        // Apply ground drag
+        if (Grounded)
+        {
+            RigidBody.drag = GroundDrag;
+        }
+        else
+        {
+            RigidBody.drag = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        HorizontalInput = Input.GetAxisRaw("Horizontal");
+        VerticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        MoveDirection = Orientation.forward * VerticalInput + Orientation.right * HorizontalInput;
+        RigidBody.AddForce(MoveDirection.normalized * PlayerSpeed * 10, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 FlatVelocity = new Vector3(RigidBody.velocity.x, 0f, RigidBody.velocity.z);
+
+        if (FlatVelocity.magnitude > PlayerSpeed)
+        {
+            Vector3 LimitedVelocity = FlatVelocity.normalized * PlayerSpeed;
+            RigidBody.velocity = new Vector3(LimitedVelocity.x, RigidBody.velocity.y, LimitedVelocity.z);
+        }
+
+    }
+}
---
+++ Submarine Game/Assets/Scripts/SubmarineControl.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubmarineControl : MonoBehaviour
+{
+    [Header("Properties")]
+    [SerializeField] private float Acceleration;
+    [SerializeField] private float MaxForwardSpeed;
+    [SerializeField] private float MaxBackwardSpeed;
+    [SerializeField] private float MinSpeed;
+    [SerializeField] private float TurnSpeed;
+    [SerializeField] private float RiseSpeed;
+    [SerializeField] private float StabilizationSmoothing;
+
+    [Header("Controls")]
+    public KeyCode Forward;
+    public KeyCode Backward;
+    public KeyCode TurnLeft;
+    public KeyCode TurnRight;
+    public KeyCode Rise;
+    public KeyCode Sink;
+
+    [Header("Necessary Variables")]
+    private float SubmarineSpeed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        ForwardAndBackwardMovement();
+        Turning();
+        RisingAndSinking();
+
+        // Submarine stabilization
+        rb.MoveRotation(Quaternion.Slerp(rb.rotation, Quaternion.Euler(new Vector3(0, rb.rotation.eulerAngles.y, 0)), StabilizationSmoothing));
+    }
+
+    private void ForwardAndBackwardMovement()
+    {
+        if (Input.GetKey(Forward))
+        {
+            SubmarineSpeed += Acceleration;
+        }
+        else if (Input.GetKey(Backward))
+        {
+            SubmarineSpeed -= Acceleration;
+        }
+        
+        if (Mathf.Abs(SubmarineSpeed) <= MinSpeed)
+        {
+            SubmarineSpeed = 0;
+        }
+        SubmarineSpeed = Mathf.Clamp(SubmarineSpeed, -MaxBackwardSpeed, MaxForwardSpeed);
+        rb.AddForce(transform.forward * SubmarineSpeed);
+    }
+
+    private void Turning()
+    {
+        if (Input.GetKey(TurnRight))
+        {
+            rb.AddTorque(transform.up * TurnSpeed);
+        }
+        else if (Input.GetKey(TurnLeft))
+        {
+            rb.AddTorque(transform.up * -TurnSpeed);
+        }
+    }
+
+    private void RisingAndSinking()
+    {
+        if (Input.GetKey(Rise))
+        {
+            rb.AddForce(transform.up * RiseSpeed);
+        }
+        else if (Input.GetKey(Sink))
+        {
+            rb.AddForce(transform.up * -RiseSpeed);
+        }
+    }
+
+}
