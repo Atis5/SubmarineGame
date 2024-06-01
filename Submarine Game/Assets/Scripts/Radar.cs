f8a7f38b7d4179a7a3f1918a9c53169d3f5b9acf
Chris <fear123450pl@o2.pl>
2024-06-01 10:20:44 +0200

    Added Radar Visuals
    - Added "Textures" and "Radar" folder
    - Added Radar-related Textures
    - Added "Radar" Script and updated it to cause Radar to rotate.

---
+++ Submarine Game/Assets/Scripts/Radar.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+
+    [SerializeField] private Transform RadarCenter;
+    [SerializeField] private float LineRotationSpeed;
+
+    private void Update()
+    {
+        RadarCenter.eulerAngles -= new Vector3(0, 0, LineRotationSpeed * Time.deltaTime);
+    }
+}
