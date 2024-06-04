ffadf8f323808c789b78b0d058640babc2225c11
Eddzey <153067797+Eddzey@users.noreply.github.com>
2024-06-04 17:08:04 +0200

    slider

--- Submarine Game/Assets/Scripts/VacuumWaste.cs
+++ Submarine Game/Assets/Scripts/VacuumWaste.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class VacuumWaste : MonoBehaviour
 {
+    [SerializeField] private Slider ProgressHandle;
+    [SerializeField] private GameObject ToxicArea;
     void Start()
     {
         
@@ -12,28 +16,31 @@ public class VacuumWaste : MonoBehaviour
     void Update()
     {
         AreaInteraction();
+        VacuumProgression();
     }
 
     //does not work atm
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "toxic")
+        if (other.gameObject.tag == "Toxic")
         {
             SubControl.SubControlScript.IsInToxicArea = true;
             Debug.Log("INSIDE TOXIC STUFF");
         }
+
     }
 
     //Should allow player to interact when inside of the toxic area
     void AreaInteraction()
     {
-        if ((SubControl.SubControlScript.IsInToxicArea == true) && (Input.GetKeyDown(Controls.ControlsScript.Vacuum)))
+        if ((SubControl.SubControlScript.IsInToxicArea == true) && (Input.GetKey(Controls.ControlsScript.Vacuum)))
         {
-            Debug.Log("Vacuum");
+            ProgressHandle.value++;
+            //Debug.Log("Vacuum");
         }
         if ((SubControl.SubControlScript.IsInToxicArea == false) && (Input.GetKeyDown(Controls.ControlsScript.Vacuum)))
         {
-            Debug.Log("No Vacuming allowed");
+            //Debug.Log("No Vacuuming allowed");
         }
     }
     private void OnTriggerExit(Collider other)
@@ -43,4 +50,19 @@ public class VacuumWaste : MonoBehaviour
             SubControl.SubControlScript.IsInToxicArea = false;
         }
     }
+
+    //retains progress of the vacuuming
+    private void VacuumProgression()
+    {
+        if (ProgressHandle.value == 1)
+        {
+            GameObject.Destroy(ToxicArea);
+        }
+    }
+    
+ // private void ()
+ // {
+ //
+ // }
+
 }
