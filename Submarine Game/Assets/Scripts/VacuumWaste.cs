54cf91c7f8888b99dfa140b03f2ce3bc912ac55f
Eddzey <153067797+Eddzey@users.noreply.github.com>
2024-06-06 23:50:08 +0200

    WASTEless :)

--- Submarine Game/Assets/Scripts/VacuumWaste.cs
+++ Submarine Game/Assets/Scripts/VacuumWaste.cs
@@ -10,6 +10,7 @@ public class VacuumWaste : MonoBehaviour
     [SerializeField] private Slider ProgressHandle;
     [SerializeField] private GameObject ToxicArea;
     [SerializeField] private GameObject Progress;
+   
     void Start()
     {
         
@@ -19,22 +20,7 @@ public class VacuumWaste : MonoBehaviour
     {
         AreaInteraction();
         VacuumProgression();
-
-  
     }
-
-
-    // UNNECESSARY \/ \/ \/ 
-    //does not work atm
-    /*private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Toxic")
-        {
-            SubControl.SubControlScript.IsInToxicArea = true;
-            //Debug.Log("INSIDE TOXIC STUFF");
-        }
-
-    }*/
 
     //Should allow player to interact when inside of the toxic area
     void AreaInteraction()
@@ -43,34 +29,15 @@ public class VacuumWaste : MonoBehaviour
         {
             Progress.SetActive(true);
             ProgressHandle.value += VacuumSpeed * Time.deltaTime;
-            //Debug.Log("Vacuum");
-        }
-        if ((SubControl.SubControlScript.IsInToxicArea == false) && (Input.GetKeyDown(Controls.ControlsScript.Vacuum)))
-        {
-            //Debug.Log("No Vacuuming allowed");
         }
     }
-
-
-    // UNNECESSARY \/ \/ \/
-    /*private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Toxic")
-        {
-            Progress.SetActive(false);
-            SubControl.SubControlScript.IsInToxicArea = false;
-        }
-    }*/
-
- 
 
     //retains progress of the vacuuming
     private void VacuumProgression()
     {
         if (ProgressHandle.value == 100)
         {
-            //Progress.SetActive(false); <-- UNNECESSARY
-            GameObject.Destroy(ToxicArea);
+            ToxicArea.SetActive(false);
             SubControl.SubControlScript.IsInToxicArea = false;
         }
 
@@ -79,10 +46,4 @@ public class VacuumWaste : MonoBehaviour
             Progress.SetActive(false);
         }
     }
-    
- // private void ()
- // {
- //
- // }
-
 }
